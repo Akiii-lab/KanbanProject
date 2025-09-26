29c8bc9759dba285b7205d16cc5a00cd9c8ed268
Akiii-lab <garciasierrandres@gmail.com>
2025-09-26 15:46:28 -0500

    feat: fix loader and add global loader

--- src/components/Loader/loader.tsx
+++ src/components/Loader/loader.tsx
@@ -8,11 +8,17 @@ export const Loader = ({ text = "Loading" }: LoaderProps) => {
     return (
         <div className="loader-wrapper">
             {text.split("").map((char, idx) => (
-                <span className="loader-letter" key={char + idx}>
+                <span
+                    className="loader-letter"
+                    key={char + idx}
+                    style={{
+                        animationDelay: `${idx * 0.1}s`,
+                    }}
+                >
                     {char === " " ? <>&nbsp;</> : char}
                 </span>
             ))}
             <div className="loader"></div>
         </div>
     );
-}
+};
