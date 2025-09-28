df69f5a3fd4337a0ebc7ea0f06787136d9a74c0a
Akiii-lab <garciasierrandres@gmail.com>
2025-09-27 22:09:26 -0500

    feat: add new function to drag and drop on columns in board

---
+++ src/components/(usercomponents)/TaskModal/TaskModal.tsx
@@ -0,0 +1,12 @@
+"use client";
+import { useBoardStore } from "@/store/boardStore";
+
+interface TaskModalProps {
+    isOpen: boolean;
+    onOpenChange: (open: boolean) => void;
+    onSubmit: (data: any) => void;
+}
+
+export const TaskModal = ({ isOpen, onOpenChange, onSubmit }: TaskModalProps) => {  
+    const { users } = useBoardStore();
+}
