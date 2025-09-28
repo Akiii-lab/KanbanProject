3c0c4bf521b70f13946f7b766df334e9b3e0eeb2
Akiii-lab <garciasierrandres@gmail.com>
2025-09-28 07:56:00 -0500

    feat: reintroduce SpeedInsights and Analytics in main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -30,6 +32,8 @@ export default function RootLayout({
       >
         {children}
         <Toaster />
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
